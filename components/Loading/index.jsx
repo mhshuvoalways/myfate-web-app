339c656cb53d8e8d926ddac0ebf2f964e8fa328e
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-10-22 13:19:16 +0600

    loading add.

--- components/Loading/index.jsx
+++ components/Loading/index.jsx
@@ -1,6 +1,6 @@
 const index = () => {
   return (
-    <p className="h-screen flex justify-center items-center text-3xl text-gray-600">
+    <p className="bg-gray-900 h-screen flex justify-center items-center text-3xl text-gray-100">
       Loading...
     </p>
   );
