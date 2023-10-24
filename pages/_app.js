c012dd8bd65d74f4ec8d18bb6315caa5d89d005c
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-10-24 21:17:43 +0600

    testimonial issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import Head from "next/head";
-import PagesLoaderContext from '@/context/usePagesLoaderContext';
 import "@/styles/external.css";
 import "@/styles/globals.css";
 import "@/styles/responsive.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <PagesLoaderContext.Provider>
+    <>
       <Head>
         <title>Explore Future</title>
       </Head>
       <Component {...pageProps} />
-    </PagesLoaderContext.Provider>
+    </>
   );
 }
