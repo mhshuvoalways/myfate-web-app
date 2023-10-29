b87018f2a781a900d6152fcbbe1cf5e9ea9ec55d
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-10-29 06:14:11 +0600

    submenu issue fixed

--- components/Headers/SubMenus.jsx
+++ components/Headers/SubMenus.jsx
@@ -1,11 +1,9 @@
-const SubMenus = ({ menuName, setMenuName }) => {
+const SubMenus = ({ menuName }) => {
   return (
     <div
       className={`w-56 absolute top-6 bg-white mt-2 border border-gray-100 rounded ${
         menuName === "explorefuture" ? "opacity-100 z-10" : "opacity-0 -z-10"
       }`}
-      onMouseEnter={() => setMenuName("explorefuture")}
-      onMouseLeave={() => setMenuName("")}
     >
       <p className="border-t border-b border-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white px-5 py-2">
         My Fate
