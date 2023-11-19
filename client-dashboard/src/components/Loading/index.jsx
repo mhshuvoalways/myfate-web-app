8fef0bdf7ef0657fa65ad4075da77272e8a4ec3a
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-11-19 15:23:03 +0600

    redirect

---
+++ client-dashboard/src/components/Loading/index.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { useSelector } from "react-redux";
+
+const ClickedLoading = () => {
+  const btnReducer = useSelector((state) => state.btnReducer);
+
+  return (
+    <>
+      {!btnReducer && (
+        <>
+          <div className="fixed inset-0 flex justify-center items-center z-[150]">
+            <Image src='/public/loading.svg' alt="" />
+          </div>
+          <p className="fixed inset-0 z-[60] opacity-60 bg-gray-900"></p>
+        </>
+      )}
+    </>
+  );
+};
+
+export default ClickedLoading;
