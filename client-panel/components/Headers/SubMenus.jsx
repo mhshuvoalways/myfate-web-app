3c151ae7541ec6f62340926986fa3db3f5bfad19
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2024-01-03 15:24:51 +0600

    dual language

--- client-panel/components/Headers/SubMenus.jsx
+++ client-panel/components/Headers/SubMenus.jsx
@@ -7,13 +7,26 @@ const SubMenus = ({ menuName, subMenusItems }) => {
         menuName === "explorefuture" ? "opacity-100 z-10" : "opacity-0 -z-10"
       }`}
     >
-      {subMenusItems.map((el) => (
-        <Link href={`/${el.replace(" ", "").toLowerCase()}`} key={el}>
-          <p className="border-t border-b border-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white px-5 py-2">
-            {el}
-          </p>
-        </Link>
-      ))}
+      <Link href={`/fullreport`}>
+        <p className="border-t border-b border-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white px-5 py-2">
+          {subMenusItems[0]}
+        </p>
+      </Link>
+      <Link href={`/dailyfate`}>
+        <p className="border-t border-b border-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white px-5 py-2">
+          {subMenusItems[1]}
+        </p>
+      </Link>
+      <Link href={`/lovefate`}>
+        <p className="border-t border-b border-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white px-5 py-2">
+          {subMenusItems[2]}
+        </p>
+      </Link>
+      <Link href={`/financefate`}>
+        <p className="border-t border-b border-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white px-5 py-2">
+          {subMenusItems[3]}
+        </p>
+      </Link>
     </div>
   );
 };
